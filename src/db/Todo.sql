34202ce91bb5ae8832bb381074b60b1adde02775
isaac <isaac.abraham@gmail.com>
2022-07-19 00:50:57 +0100

    Database project

--- src/db/Todo.sql
+++ src/db/Todo.sql
@@ -1,9 +1,8 @@
-
 CREATE TABLE dbo.Todo
 (
-    Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-    Title nvarchar(255) NOT NULL,
-    Description nvarchar(255) NULL,
-    CreatedDate datetime NOT NULL,
-    CompletedDate datetime NULL
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [Title] nvarchar(255) NOT NULL,
+    [Description] nvarchar(255) NULL,
+    [CreatedDate] datetime NOT NULL,
+    [CompletedDate] datetime NULL
 );
