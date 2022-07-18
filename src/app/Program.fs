611d4babab903714a8e833699b281fdcf078e986
isaac <isaac.abraham@gmail.com>
2022-07-18 21:44:57 +0100

    Cleanup

--- src/app/Program.fs
+++ src/app/Program.fs
@@ -1,63 +1,25 @@
 ﻿open Giraffe
-open Microsoft.AspNetCore.Http
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
-open Saturn
-open System.Text.Json
-open System.Text.Json.Serialization
-open System.Collections.Generic
 open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Hosting
+open Saturn
+open System.Collections.Generic
+open System.Text.Encodings.Web
+open System.Text.Json
 
-
-type CustomerDatabase() =
-    let customers = Dictionary<int, {| Name: string |}>()
-
-    member _.LoadCustomer(customerId: int) =
-        if customers.ContainsKey customerId then
-            customers.[customerId]
-        else
-            let fromDb = {| Name = "Isaac" |}
-            customers[customerId] <- fromDb
-            fromDb
-
-type CustomerService(db: CustomerDatabase, logger: ILogger) =
-    member _.TryLoadCustomer(customerId: int) =
-        if customerId < 99 then
-            failwith "Bad request"
-        else
-            db.LoadCustomer(customerId)
-
-
-let configureServices (services: IServiceCollection) =
-    // IOC Containers (Inversion of Control Container)
-
-    services.AddTransient<CustomerDatabase>()
-    |> ignore
-
-    let service =
-        services
-            .BuildServiceProvider()
-            .GetService<CustomerService>()
-
-    let c = service.TryLoadCustomer 123
-
-    services.AddSingleton<Json.ISerializer>(
-        SystemTextJson.Serializer(
-            let options =
-                JsonSerializerOptions(
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase, // Emit camel case JSON
-                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping // Allow unescaped JSON
-                )
-            // options.Converters.Add(JsonFSharpConverter(allowNullFields = true)) // Nice F# JSON serialization
-            options
+let serializer =
+    let options =
+        JsonSerializerOptions(
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase, // Emit camel case JSON
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping // Allow unescaped JSON
         )
-    )
-
+    // options.Converters.Add(JsonFSharpConverter(allowNullFields = true)) // Nice F# JSON serialization
+    SystemTextJson.Serializer options
 
 let app =
     application {
         use_router Todo.Api.giraffeRouter
-        service_config configureServices
+        use_json_serializer serializer
     }
 
 run app
